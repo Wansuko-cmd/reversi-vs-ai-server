b1d59df57e7651eb71448b23a5c0db72eec4a06d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-03 13:42:03 +0900

    [add] gradleの設定を追加

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+@file:Suppress("UnstableApiUsage")
+enableFeaturePreview("VERSION_CATALOGS")
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+rootProject.name = "reversi-server"
