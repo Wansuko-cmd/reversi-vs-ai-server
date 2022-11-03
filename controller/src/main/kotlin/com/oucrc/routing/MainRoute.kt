5628a02091d140aaebc896423b9d2be8f81d524f
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-03 17:29:45 +0900

    [add] Rooms Index Postを追加

--- controller/src/main/kotlin/com/oucrc/routing/MainRoute.kt
+++ controller/src/main/kotlin/com/oucrc/routing/MainRoute.kt
@@ -2,6 +2,7 @@ package com.oucrc.routing
 
 import com.oucrc.routing.ai.index.aisIndexGet
 import com.oucrc.routing.rooms.index.roomsIndexGet
+import com.oucrc.routing.rooms.index.roomsIndexPost
 import com.oucrc.routing.rooms.show.roomsShowGet
 import com.oucrc.routing.rooms.show.roomsShowPost
 import com.oucrc.routing.users.index.usersIndexGet
@@ -18,6 +19,7 @@ fun Application.mainRoute() {
     routing {
         route(Routing.Room.path) {
             roomsIndexGet(path = Routing.Room.Index.path)
+            roomsIndexPost(path = Routing.Room.Index.path)
             roomsShowGet(
                 path = Routing.Room.Show.path,
                 param = Routing.Room.Show.roomId,
---
+++ controller/src/main/kotlin/com/oucrc/routing/rooms/index/RoomsIndexPostRoute.kt
@@ -0,0 +1,38 @@
+package com.oucrc.routing.rooms.index
+
+import com.oucrc.ext.getRequest
+import com.oucrc.serializable.ExceptionSerializable
+import com.wsr.result.consume
+import com.wsr.result.flatMap
+import com.wsr.result.mapFailure
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.post
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.koin.ktor.ext.inject
+import player.PlayerId
+import room.CreateRoomUseCase
+
+fun Route.roomsIndexPost(path: String) {
+    val createRoomUseCase by inject<CreateRoomUseCase>()
+
+    post(path) {
+        call.getRequest<RoomsIndexPostRequest>("Invalid request.")
+            .flatMap { (aiId, userId,) ->
+                createRoomUseCase(aiId = PlayerId.AiId(aiId), userId = PlayerId.UserId(userId))
+            }
+            .mapFailure { ExceptionSerializable.from(it) }
+            .consume(
+                success = { room -> call.respond(room) },
+                failure = { (message, status) -> call.respond(status, message) }
+            )
+    }
+}
+
+@Serializable
+data class RoomsIndexPostRequest(
+    @SerialName("ai_id") val aiId: String,
+    @SerialName("user_id") val userId: String,
+)
